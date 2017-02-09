f8ceabf52cbc8da0da30c58c87221a44dc34562c
Andrew Pealing <roo@norwegian.blue>
2017-02-09 13:04:05 +1300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# vanilla
+This is a personal repo for quickly creating new Drupal 8 websites on my local machine for development.<br>
+The scripts use standard D8 composer installation process,
+copies a set of files such as settings.php customised to my use,
+and installs some modules I usually use using composer.<br>
+It also adds the entry to the aliases.drushrc.php file assuming the user running
+the script has access rights to the file.<br>
+git is also initialised in the new site's directory and a gitignore created.<br>
+The scripts are bash and use kdialog, so assume kde is installed.
---
+++ drupal-8.x/web/sites/newsite/settings.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * @file
+ * Drupal site-specific configuration file.
+ *
+ * IMPORTANT NOTE:
+ *
+ * @see example.sites.php
+ * for documentation
+ *
+ */
+
+/**
+ * Salt for one-time login links, cancel links, form tokens, etc.
+ */
+$settings['hash_salt'] = '8tBJjJINM0gdOw9xB8zbSihwwQto8EDgEEJaDxbYzudlPPdPM2dkN7FgiAHCO7dtyB4E3c2NcA';
+
+
+/**
+ * Access control for update.php script.
+ */
+$settings['update_free_access'] = FALSE;
+
+/**
+ * Default mode for for directories and files written by Drupal.
+ *
+ * Value should be in PHP Octal Notation, with leading zero.
+ */
+# $settings['file_chmod_directory'] = 0775;
+# $settings['file_chmod_file'] = 0664;
+
+/**
+ * Public file path:
+ */
+# $settings['file_public_path'] = 'sites/default/files';
+
+/**
+ * Private file path:
+ */
+$settings['file_private_path'] = '/var/www/vhosts/newsite/private';
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+/**
+ * Database settings:
+ */
+
+$databases['default']['default'] = array (
+  'database' => 'databasename',
+  'username' => 'databaseuser',
+  'password' => 'password',
+  'prefix' => 'newsite_',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = 'config/sync';
+
+/**
+ * Load local development override configuration, if available.
+ *
+ * Use settings.local.php to override variables on secondary (staging,
+ * development, etc) installations of this site. Typically used to disable
+ * caching, JavaScript/CSS compression, re-routing of outgoing emails, and
+ * other things that should not happen on development and testing sites.
+ *
+ * Keep this code block at the end of this file to take full effect.
+ */
+ if (file_exists(__DIR__ . '/settings.local.php')) {
+   include __DIR__ . '/settings.local.php';
+ }
